b9d772973d28301574442b453727293fa93edb73
garvsharma21 <garvsh7@gmail.com>
2026-02-07 22:18:05 +0530

    Made changes to requirements.txt

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,11 +1,12 @@
+import os
 from extract import extract_data
 from transform import transform_data
 from load import load_data
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def run_pipeline():
-    data = extract_data("data/daily_gym_attendance_workout_data.csv")
+    data = extract_data(os.getenv("SOURCE-FILE-PATH"))
     data = transform_data(data)
-    load_data(data, "data/output.csv")
-
-if __name__ == "__main__":
-    run_pipeline()
+    load_data(data, os.getenv("SINK-FILE-PATH"))
